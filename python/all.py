1c1ee93cc98f883d98286e013ebbe640daadeb7d
filippodelministro <filippodelministro@gmail.com>
2024-01-03 14:08:44 +0100

    removed rubbish

--- python/all.py
+++ python/all.py
@@ -13,23 +13,13 @@ pswd_list = []   # list of pswd_objects
 
 def init():
 
-    # Workbook() takes one, non-optional, argument 
-    # which is the filename that we want to create.
-    workbook = xlsxwriter.Workbook('hello.xlsx')
+    workbook = xlsxwriter.Workbook('pswd.xlsx')
 
-    # The workbook object is then used to add new 
-    # worksheet via the add_worksheet() method.
     worksheet = workbook.add_worksheet()
 
-    # Use the worksheet object to write
-    # data via the write() method.
-    worksheet.write('A1', 'Hello..')
-    worksheet.write('B1', 'Geeks')
-    worksheet.write('C1', 'For')
-    worksheet.write('D1', 'Geeks')
+    worksheet.write('A1', 'Service')
+    worksheet.write('B1', 'Password')
 
-    # Finally, close the Excel file
-    # via the close() method.
     workbook.close()
 
 
